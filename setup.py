6303450c3011f39e4b6e7d2bddf88c99525986b5
James <jamesj0107@naver.com>
2022-10-30 03:10:25 +0900

    [OD15-100] chore: Update to version 0.2.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as readme:
 
 setuptools.setup(
     name='datanashor',
-    version='0.2.1',
+    version='0.2.3',
     author='James Jung',
     author_email='therealjamesjung@gmail.com',
     description='League of Legends live replay parser',
@@ -17,7 +17,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     keyword=['League of Legends', 'Replay', 'Parser', 'League',
              'Lol', 'Lolreplay', 'Lolreplayparser', 'Lolreplayparser', 'DataNashor', 'datanashor'],
-    dependencies=['requests', 'beautifulsoup4', 'pyserial'],
+    install_requires=['requests', 'beautifulsoup4', 'pyserial'],
     classifiers=[
         'Intended Audience :: Developers',
         'Programming Language :: Python :: 3',
