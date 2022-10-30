f926953a9482a3a43316dc9006859fc0e2b27516
James <jamesj0107@naver.com>
2022-10-31 01:22:36 +0900

    [OD15-100] feat: parse client metadata

--- datanashor/client.py
+++ datanashor/client.py
@@ -1,3 +1,4 @@
+import base64
 import subprocess
 import os
 
@@ -36,3 +37,22 @@ class ReplayClient():
         )
 
         return process
+
+    def get_client_metadata(self):
+        '''
+        Returns the client metadata.
+        League of Legends client must be running.
+        NOTE: by client, it does not means the replay client, but the actual game client.
+        '''
+        try:
+            raw_metadata = open(self.game_dir + '\\lockfile',
+                                'r').read().split(':')
+            metadata = {
+                "port": raw_metadata[2],
+                "token": str(base64.b64encode(f'riot:{raw_metadata[3]}'.encode('utf-8')), encoding='utf-8'),
+            }
+
+            return metadata
+        except FileNotFoundError:
+            raise FileNotFoundError(
+                'lockfile not found. Is the client running?')
