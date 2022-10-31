952d828a49efaa8195d0b6c7446b900f64c94522
James Jung <therealjamesjung@gmail.com>
2022-10-31 23:46:19 +0900

    [OD15-100] chore: Update to version 0.3.4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as readme:
 
 setuptools.setup(
     name='datanashor',
-    version='0.3.1',
+    version='0.3.4',
     author='James Jung',
     author_email='therealjamesjung@gmail.com',
     description='League of Legends live replay parser',
